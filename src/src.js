f8d1d8d86e01698fcd465f76cd0f7440c56584fc
PAWA <PAWA@dr.dk>
2018-10-08 08:56:06 +0200

    doc adjustments

--- src/src.js
+++ src/src.js
@@ -7,23 +7,27 @@ import { VideoPlayer } from '@dr/elements';
 class MyApp extends Component {
   constructor(props) {
     super(props);
+    // make the ref available on the component object.
     this.videoPlayerRef = React.createRef();
+    this.handleReadyEvent = this.handleReadyEvent.bind(this);
+    this.handlePlayEvent = this.handlePlayEvent.bind(this);
   }
 
   handleReadyEvent() {
     // Once player ready has been called, 
     // the playerActions object will be available on the videoplayer ref.
     const playerActions = this.videoPlayerRef.current.playerActions;
-    playerActions.pause();
-    playerActions.play();
+    console.log("ready event", playerActions);
     // playerActions.seek(30) // seconds
     // playerActions.duration() // returns the length of the stream in seconds.
     // playeractions.currentTime() // returns how many seconds into the stream, the media has played.
 
   }
   handlePlayEvent() {
-    //player event is caught here
-    console.log("play event");
+    let playerActions = this.videoPlayerRef.current.playerActions;
+    // the play event is always triggered after ready, so playerActions should always be available here.
+    console.log("play event triggered, pausing player");
+    playerActions.pause();
   }
   
   render() {
@@ -31,8 +35,7 @@ class MyApp extends Component {
       <div>
         <VideoPlayer
           resource="urn:dr:mu:programcard:5a78b748a11f9f171cc3bfcb"
-          // offset, sets amount of seconds into the program, a stream should start.
-          offset={20}
+          
           // ref allows you to access the playerActions object on the videoplayer reference.
           // these actions cant be called before the player ready event has been triggered.
           // actions include: play, pause, seek, duration, currentTime
@@ -44,6 +47,9 @@ class MyApp extends Component {
           // onPause={this.handlePauseEvent}
           // onTimeupdate={this.handleTimeupdatedEvent}
           // onEnded={this.handleEndedEvent}
+          autoplay={true}
+          // offset, sets amount of seconds into the program, a stream should start.
+          offset={20}
           />
         This text should not jump when player is loaded
         </div>
